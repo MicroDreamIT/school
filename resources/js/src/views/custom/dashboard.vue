a637493445d55020e88f6e044d353139df703c67
hasib <hasib.arman.08@gmail.com>
2020-02-25 13:35:18 +0600

    student update

--- resources/js/src/views/custom/dashboard.vue
+++ resources/js/src/views/custom/dashboard.vue
@@ -5,14 +5,41 @@
         </h1>
         <div class="row">
             <div class="col-md-10">
-
                 <chartjs-component-line-chart :chartdata="data"
-                                              :options="options" :style="myStyles"></chartjs-component-line-chart>
+                                              :options="options" :style="myStyles">
+
+                </chartjs-component-line-chart>
             </div>
             <div class="col-md-2">
-
                 <chartjs-component-line-chart :chartdata="data" :style="myStyles"
-                                              :options="options"></chartjs-component-line-chart>
+                                              :options="options">
+                </chartjs-component-line-chart>
+            </div>
+            <div class="col-md-8">
+                <data-table :headers="tableHeader1"
+                            :url="'/student'"
+                            :no-data-message="'No matching records found'"
+                            :searchField="searchData"
+                            :hasSearch="false"
+                >
+                    <template slot="items" slot-scope="props">
+                        <vs-td :data="props.data.username">
+                            {{props.data.email}}
+                        </vs-td>
+
+                        <vs-td :data="props.data.username">
+                            {{props.data.username}}
+                        </vs-td>
+
+                        <vs-td :data="props.data.id">
+                            {{props.data.website}}
+                        </vs-td>
+
+                        <vs-td :data="props.id">
+                            {{props.data.id}}
+                        </vs-td>
+                    </template>
+                </data-table>
             </div>
 
         </div>
@@ -21,8 +48,13 @@
 </template>
 <script>
     import ChartjsComponentLineChart from "./component/chart/LineChart"
+    import DataTable from './component/table/data-table'
 
     export default {
+        components: {
+            ChartjsComponentLineChart,
+            DataTable
+        },
         data() {
             return {
                 data: {
@@ -49,16 +81,25 @@
                         xAxes: [{
                             gridLines: {
                                 offsetGridLines: true
-                            }}
+                            }
+                        }
                         ]
 
-                }
+                    }
+                },
+                tableHeader1: [
+                    {name: 'Name', field: 'name',sort_key:'name'},
+                    {name: 'Email', field: 'email',sort_key:'email'},
+                    {name: 'Mobile', field: 'mobile'},
+                    {name: 'PID'},
+                    {name: 'File Number'},
+                    {name: 'Status'},
+                    {name: 'Action'},
+                ],
+                searchData:{}
             }
-        }
         },
-        components: {
-            ChartjsComponentLineChart
-        },
+
         computed: {
             myStyles() {
                 return {

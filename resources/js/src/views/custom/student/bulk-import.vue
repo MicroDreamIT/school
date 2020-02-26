7f90948f1f23c1ccc9cab3e428d4bf3e3f7cdf6b
ashiqulemu.jpi@gmail.com <ashiqulemu.jpi@gmail.com>
2020-02-26 17:45:01 +0600

    sidebar modified

--- resources/js/src/views/custom/student/bulk-import.vue
+++ resources/js/src/views/custom/student/bulk-import.vue
@@ -2,27 +2,55 @@
     <div>
         <div class="row">
             <div class="col-md-12 mb-2">
-                <h2 class="pageTitle">Details</h2>
+                <h2 class="pageTitle">Import</h2>
             </div>
             <div class="col-md-12">
                 <div class="row mx-0">
                     <router-link :to="'/student'">
-                        <vs-button type="filled" class="smBtn" icon="list">Primary</vs-button>
+                        <vs-button type="filled" class="smBtn" icon="list">
+                            <i class="fa fa-list" aria-hidden="true"></i>
+                            Detail</vs-button>
                     </router-link>
                     <router-link :to="'/'">
-                        <vs-button type="filled" class="smBtn">Primary</vs-button>
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-plus" aria-hidden="true"></i>
+                            Registration</vs-button>
+                    </router-link>
+                    <router-link :to="'/student/import'">
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-upload" aria-hidden="true"></i>
+                            Bulk Registration</vs-button>
+                    </router-link>
+                    <router-link :to="'/transfer-student'">
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-exchange" aria-hidden="true"></i>
+                            Transfer</vs-button>
+                    </router-link>
+                    <router-link :to="'/doc-upload'">
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-files-o" aria-hidden="true"></i>
+                            Documents</vs-button>
+                    </router-link>
+                    <router-link :to="'/create-notes'">
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-sticky-note" aria-hidden="true"></i>
+                            Notes
+                        </vs-button>
                     </router-link>
                     <router-link :to="'/'">
-                        <vs-button type="filled" class="smBtn">Primary</vs-button>
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-calculator" aria-hidden="true"></i>
+                            Balance Fees</vs-button>
                     </router-link>
                     <router-link :to="'/'">
-                        <vs-button type="filled" class="smBtn">Primary</vs-button>
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-calculator" aria-hidden="true"></i>
+                            Library</vs-button>
                     </router-link>
                     <router-link :to="'/'">
-                        <vs-button type="filled" class="smBtn">Primary</vs-button>
-                    </router-link>
-                    <router-link :to="'/'">
-                        <vs-button type="filled" class="smBtn">Primary</vs-button>
+                        <vs-button type="filled" class="smBtn">
+                            <i class="fa fa-calendar" aria-hidden="true"></i>
+                            Attendance</vs-button>
                     </router-link>
                 </div>
             </div>
@@ -39,16 +67,17 @@
             <div class="col-md-12">
                 <vs-row vs-justify="center">
                     <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="17">
-                        <vs-card>
-                            <div>
-                                <span>Import</span>
+                        <vs-card class="p-4">
+                                <h4>Import</h4>
                                 <hr>
-                                <span>CSV Template for Bulk Student Import</span>
+                                <h4 class="text-capitalize">
+                                    <i class="fa fa-download"></i> CSV Template for Bulk Student Import
+                                </h4>
                                 <hr>
                                 <vs-input type="file"></vs-input>
                                 <hr>
-                                <vs-button color="primary" type="filled">Upload</vs-button>
-                            </div>
+                                <vs-button color="primary" class="rounded" type="filled">Upload</vs-button>
+                          
                         </vs-card>
                     </vs-col>
                 </vs-row>

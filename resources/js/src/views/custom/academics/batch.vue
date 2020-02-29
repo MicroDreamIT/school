6b692d8a8461e022e366f38714d5a92975bafcf1
ashiqulemu <ashiqulemu.jpi@gmail.com>
2020-02-29 13:17:44 +0600

    sidebar modified

--- resources/js/src/views/custom/academics/batch.vue
+++ resources/js/src/views/custom/academics/batch.vue
@@ -3,86 +3,94 @@
         <div class="row ">
             <div class="col-md-12">
                 <h2 class="pageTitle">Student Batch Manager</h2>
-                <h2 class="pageTitle">notification bar</h2>
-            </div>
-            <vs-card>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div>Create Student Batch</div>
-                        <div class="d-flex justify-content-between">
-                            <p>Status</p>
-                            <vs-input></vs-input>
-                        </div>
-                        <vs-divider></vs-divider>
-                        <vs-button color="#00b8cf"
-                                   type="filled"
-                                   class="my-round">Create
-                        </vs-button>
-                    </div>
-                    <div class="col-md-8">
-                        <h4 class="header large lighter blue">
-                            <i class="fa fa-list" aria-hidden="true"></i>&nbsp; Student Batch List
-                            List</h4>
-                        <div class="clearfix mt-3">
-                            <div class="easy-link-menu">
-                                <a class="btn-success btn-sm bulk-action-btn">
-                                    <i class="fa fa-check" aria-hidden="true"></i> Active</a>
-                                <a class="btn-warning btn-sm bulk-action-btn">
-                                    <i class="fa fa-remove" aria-hidden="true"></i>
-                                    In-Active</a>
-                                <a class="btn-danger btn-sm bulk-action-btn">
-                                    <i class="fa fa-trash" aria-hidden="true"></i> Delete</a>
-                            </div>
-                        </div>
-                        <br>
-                        <div class="table-header">
-                           Student Batch Record list on table. Filter Student Batch using the filter.
-                        </div>
-                        <div class="dt-buttons btn-group action-group mt-3">
-                            <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0"
-                                    aria-controls="DataTables_Table_0">
-                                <span>Copy</span></button>
-                            <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0"
-                                    aria-controls="DataTables_Table_0">
-                                <span>PDF</span>
-                            </button>
-                            <button class="btn btn-secondary" tabindex="0" aria-controls="DataTables_Table_0">
-                                <span>JSON</span>
-                            </button>
-                            <button class="btn btn-secondary buttons-print" tabindex="0"
-                                    aria-controls="DataTables_Table_0">
-                                <span>Print</span>
-                            </button>
-                        </div>
-                        <data-table :headers="tableHeader"
-                                    :url="'/student'"
-                                    :no-data-message="'No Day data found. Please Filter Day to show.'"
-                                    :searchField="searchData"
-                                    :hasSearch="true"
-                                    :has-multiple="true"
-                        >
-                            <template slot="items" slot-scope="props">
-                                <vs-td :data="props.data.username" class="pointer-none">
-                                    {{props.data.email}}
-                                </vs-td>
-
-                                <vs-td :data="props.data.username">
-                                    {{props.data.username}}
-                                </vs-td>
-
-                                <vs-td :data="props.data.id">
-                                    {{props.data.website}}
-                                </vs-td>
-
-                                <vs-td :data="props.id">
-                                    {{props.data.id}}
-                                </vs-td>
-                            </template>
-                        </data-table>
-                    </div>
+                <div  role="alert" class="mt-2 alert alert-success alert-dismissible display-block">
+                    <button type="button" data-dismiss="alert" aria-label="Close" class="close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                    <i class="ace-icon fa fa-hand-o-right"></i>
+                    Please, Create Year and Active
                 </div>
-
-            </vs-card>
+            </div>
+            <div class="col-md-12">
+                <vs-card>
+                    <div class="row p-4"><br>
+                        <div class="col-md-4">
+                            <h4> <i class="fa fa-search"></i> Create Student Batch</h4><br>
+                            <div class="forn-group row">
+                                <label class="col-sm-3">Status</label>
+                                <vs-input class="col-sm-9"></vs-input>
+                            </div>
+                            <vs-divider></vs-divider>
+                            <vs-button color="#00b8cf"
+                                       type="filled"
+                                       class="my-round">Create
+                            </vs-button>
+                        </div>
+                        <div class="col-md-8">
+                            <h4 class="header large lighter blue">
+                                <i class="fa fa-list" aria-hidden="true"></i>&nbsp; Student Batch List
+                                List</h4>
+                            <div class="clearfix mt-3">
+                                <div class="easy-link-menu">
+                                    <a class="btn-success btn-sm bulk-action-btn">
+                                        <i class="fa fa-check" aria-hidden="true"></i> Active</a>
+                                    <a class="btn-warning btn-sm bulk-action-btn">
+                                        <i class="fa fa-remove" aria-hidden="true"></i>
+                                        In-Active</a>
+                                    <a class="btn-danger btn-sm bulk-action-btn">
+                                        <i class="fa fa-trash" aria-hidden="true"></i> Delete</a>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="table-header">
+                                Student Batch Record list on table. Filter Student Batch using the filter.
+                            </div>
+                            <div class="dt-buttons btn-group action-group mt-3">
+                                <button class="btn btn-secondary buttons-copy buttons-html5" tabindex="0"
+                                        aria-controls="DataTables_Table_0">
+                                    <span>Copy</span></button>
+                                <button class="btn btn-secondary buttons-pdf buttons-html5" tabindex="0"
+                                        aria-controls="DataTables_Table_0">
+                                    <span>PDF</span>
+                                </button>
+                                <button class="btn btn-secondary" tabindex="0" aria-controls="DataTables_Table_0">
+                                    <span>JSON</span>
+                                </button>
+                                <button class="btn btn-secondary buttons-print" tabindex="0"
+                                        aria-controls="DataTables_Table_0">
+                                    <span>Print</span>
+                                </button>
+                            </div>
+                            <data-table :headers="tableHeader"
+                                        :url="'/student'"
+                                        :no-data-message="'No Day data found. Please Filter Day to show.'"
+                                        :searchField="searchData"
+                                        :hasSearch="true"
+                                        :has-multiple="true"
+                            >
+                                <template slot="items" slot-scope="props">
+                                    <vs-td :data="props.data.username" class="pointer-none">
+                                        {{props.data.email}}
+                                    </vs-td>
+                        
+                                    <vs-td :data="props.data.username">
+                                        {{props.data.username}}
+                                    </vs-td>
+                        
+                                    <vs-td :data="props.data.id">
+                                        {{props.data.website}}
+                                    </vs-td>
+                        
+                                    <vs-td :data="props.id">
+                                        {{props.data.id}}
+                                    </vs-td>
+                                </template>
+                            </data-table>
+                        </div>
+                    </div>
+    
+                </vs-card>
+            </div>
         </div>
     </div>
 </template>

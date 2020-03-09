8899cf381e605bed9943ae1566c04cb4415c7a94
ashiqulemu <ashiqulemu.jpi@gmail.com>
2020-03-09 14:00:16 +0600

    overall

--- resources/js/src/views/custom/library/books/create.vue
+++ resources/js/src/views/custom/library/books/create.vue
@@ -4,7 +4,7 @@
 			<div class="col-md-12 mb-2">
 				<h2 class="pageTitle"> Books Manager </h2>
 			</div>
-			<div class="col-md-12">
+			<div class="col-md-12 ">
 				<div class="row mx-0">
 					<router-link :to="'/student'">
 						<vs-button type="filled" class="smBtn">
@@ -60,74 +60,177 @@
 					{{hasNotification}}
 				</div>
 			</div>
-			<vs-divider class="mx-3"/>
-			<div class="col-md-12">
+			<div class="col-md-12 mt-3">
 				<vs-card>
 					<div class="row p-4">
-						<router-link :to="'/student'">
-							<vs-button type="filled" class="smBtn">
-								<i class="fa fa-book" aria-hidden="true"></i>
-								Book Detail
-							</vs-button>
-						</router-link>
-						<router-link :to="'/student/registration'">
-							<vs-button type="filled" class="smBtn">
-								<i class="fa fa-plus" aria-hidden="true"></i>
-								New Book
-							</vs-button>
-						</router-link>
-						<router-link :to="'/student/import'">
+						<div>
+							<router-link :to="'/student'">
+								<vs-button type="filled" class="smBtn">
+									<i class="fa fa-book" aria-hidden="true"></i>
+									Book Detail
+								</vs-button>
+							</router-link>
+							<router-link :to="'/student/registration'">
+								<vs-button type="filled" class="smBtn">
+									<i class="fa fa-plus" aria-hidden="true"></i>
+									New Book
+								</vs-button>
+							</router-link>
+							<router-link :to="'/student/import'">
+								<vs-button type="filled" class="smBtn">
+									<i class="fa fa-upload" aria-hidden="true"></i>
+									Bulk Import
+								</vs-button>
+							</router-link>
+							<router-link :to="'/student/transfer'">
+								<vs-button type="filled" class="smBtn">
+									<i class="fa fa-exchange" aria-hidden="true"></i>
+									Book Category
+								</vs-button>
+							</router-link>
+						</div>
+						<vs-divider class="mx-1"/>
+						<h4 class="d-inline-block mr-4"> <i class="fa fa-plus"></i> Books </h4>
+						<router-link :to="'/'">
 							<vs-button type="filled" class="smBtn">
 								<i class="fa fa-upload" aria-hidden="true"></i>
-								Bulk Import
-							</vs-button>
-						</router-link>
-						<router-link :to="'/student/transfer'">
-							<vs-button type="filled" class="smBtn">
-								<i class="fa fa-exchange" aria-hidden="true"></i>
-								Book Category
+								Bulk Import From CSV
 							</vs-button>
 						</router-link>
 					</div>
-					<hr class="own-hr">
-					<div class="row">
-					
+					<div class="row px-4">
+						<div class="col-md-4">
+							<div class="form-group ">
+								<label >ISBN Number</label>
+								<vs-input class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+							<label >Code</label>
+							<vs-input  class="w-100"></vs-input>
+						   </div>
+						
+							<div class="form-group ">
+								<label > Book Name</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group">
+								<label >Category </label>
+								<vs-select  class="w-100"></vs-select>
+							</div>
+							<div class="form-group ">
+								<label >Language</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Book Status </label>
+								<vs-select  class="w-100"></vs-select>
+							</div>
+							<div class="form-group ">
+								<label >Book Image </label>
+								<vs-input  class="w-100" type="file"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Notes </label>
+								<vs-textarea  class="w-100"></vs-textarea>
+							</div>
+						</div>
+						<div class="col-md-4">
+							
+							<div class="form-group ">
+								<label >Start</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Start Code</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Sub Title</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+						
+							<div class="form-group ">
+								<label >Edition</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+						
+							<div class="form-group ">
+								<label >Series</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Rack Location </label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Price  </label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Page   </label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="form-group ">
+								<label >End</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >End Code</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label > Total Quantity </label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							
+							<div class="form-group ">
+								<label >Edition Year</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Publisher</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Author</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+							<div class="form-group ">
+								<label >Publish Year</label>
+								<vs-input class="w-100"></vs-input>
+							</div>
+						
+							<div class="form-group ">
+								<label >Source</label>
+								<vs-input  class="w-100"></vs-input>
+							</div>
+						</div>
 					</div>
+					<vs-divider class="mx-1"/>
+					<div class="row p-3">
+						<div class="col-md-12">
+							<button class="btn" type="reset">
+								<i class="fa fa-undo"></i>
+								Reset
+							</button>
+							<button class="btn btn-primary " type="submit"  >
+								<i class="fa fa-save "></i>
+								Save
+							</button>
+							<button class="btn btn-success " type="submit"  >
+								<i class="fa fa-save "></i>
+								<i class="fa fa-plus "></i>
+								Save And Add Another
+							</button>
+						</div>
+					</div>
+				 
 				</vs-card>
 			</div>
 		</div>
-		<vs-popup class="holamundo"
-		          :title="residentUser.reg_no+' | Manage Resident'"
-		          :active.sync="residentModal">
-			
-			<div class="form-group mt-3">
-				<label class="col-sm-12">Hostel</label>
-				<vs-select class="col-sm-12"></vs-select>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-12">Room</label>
-				<vs-select class="col-sm-12"></vs-select>
-			</div>
-			<div class="form-group">
-				<label class="col-sm-12">Bed</label>
-				<vs-select class="col-sm-12"></vs-select>
-			</div>
-			<div class="footer-modal">
-				<button type="button"
-				        class="btn btn-default pull-right mr-1"
-				        @click="$emit('close')">
-					Cancel
-				</button>
-				<button type="button" class="btn btn-success pull-right">
-					Assign Bed
-				</button>
-			</div>
-		</vs-popup>
-		<vs-popup class="holamundo"
-		          :title="transportUser.reg_no+' | Manage Transport User'"
-		          :active.sync="transportModal">
-		
-		</vs-popup>
+	 
 	</div>
 </template>
 

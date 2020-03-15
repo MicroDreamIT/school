375f54580274070f1b442216f08ccc751c90572e
hasib <hasib.arman.08@gmail.com>
2020-03-15 11:10:57 +0600

    examination

--- resources/js/src/views/custom/component/navbar/nav-custom-dropdown.vue
+++ resources/js/src/views/custom/component/navbar/nav-custom-dropdown.vue
@@ -8,37 +8,37 @@
             <vs-dropdown-menu class="vx-navbar-dropdown">
                 <ul>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/user').catch(() => {})">
                         <feather-icon icon="UserIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">User & Roles</small>
                     </li>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/super-suit/user-activity').catch(() => {})">
                         <feather-icon icon="ActivityIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">User Activity</small>
                     </li>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/setting/general').catch(() => {})">
                         <feather-icon icon="SettingsIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">General Setting</small>
                     </li>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/setting/sms').catch(() => {})">
                         <feather-icon icon="MessageCircleIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">SMS Setting</small>
                     </li>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/setting/email').catch(() => {})">
                         <feather-icon icon="MailIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">Email Setting</small>
                     </li>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/setting/alert').catch(() => {})">
                         <feather-icon icon="BellIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">Alert Setting</small>
                     </li>
                     <li  class="flex py-2 px-4 cursor-pointer hover:bg-primary hover:text-white"
-                            @click="$router.push('/pages/profile').catch(() => {})">
+                            @click="$router.push('/setting/payment-gateway').catch(() => {})">
                         <feather-icon icon="CreditCardIcon" svgClasses="w-4 h-4"/>
                         <small class="ml-2 ">Payment Gateway</small>
                     </li>

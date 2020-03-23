75419cc99cde518520f60d5017882f8c1afadfbb
hasib <hasib.arman.08@gmail.com>
2020-03-23 11:52:57 +0600

    payment

--- resources/js/src/views/custom/academics/paymentEdit.vue
+++ resources/js/src/views/custom/academics/paymentEdit.vue
@@ -3,18 +3,21 @@
         <div class="row ">
             <div class="col-md-12">
                 <h2 class="pageTitle">Payment Method Manager</h2>
-                <div role="alert" class="mt-2 alert alert-success alert-dismissible display-block"
-                     v-if="notification">
+            </div>
+            <div class="col-md-12" v-if="$root.notification.status">
+                <div role="alert"
+                     :class="`mt-2 alert alert-${$root.notification.status} alert-dismissible display-block`"
+                >
                     <button type="button"
                             data-dismiss="alert"
                             aria-label="Close"
                             class="close"
-                            @click="notification=''"
+                            @click="$root.emptyNotification()"
                     >
                         <span aria-hidden="true">×</span>
                     </button>
                     <i class="ace-icon fa fa-hand-o-right"></i>
-                    {{notification}}
+                    {{$root.notification.message}}
                 </div>
             </div>
             <vs-divider class="mx-3"></vs-divider>
@@ -22,76 +25,107 @@
                 <vs-card>
                     <div class="row p-4">
                         <div class="col-md-4">
-                            <h4> <span v-if="method!='Edit'">Create</span>
-                                <span v-else>Update</span> Payment Method</h4><br>
-                            <div class="form-group row mb-3">
-                                <label class="col-sm-3"> Status</label>
-                                <vs-input
-                                        class="col-sm-9"
-                                        v-model="status"
-                                ></vs-input>
+                            <h4>
+                                <i class="fa fa-search"></i>
+                                Create Payment Method</h4>
+                            <br>
+                            <div class="form-group row">
+                                <label class="col-md-4">Title</label>
+                                <vs-input class="col-md-8"
+                                          v-model="title"
+                                          placeholder=""
+                                          v-validate="'required'"
+                                          data-vv-name="title"
+                                          :danger="errors.first('title')?true:false"
+                                          :danger-text="errors.first('title')"
+                                >
+
+                                </vs-input>
                             </div>
-                            <vs-divider></vs-divider>
-                            <vs-button color="#00b8cf"
-                                       type="filled"
-                                       class="my-round"
-                                       v-if="method=='Edit'"
-                                       @click.prevent="update"
-                            >
-                                Update
-                            </vs-button>
-                            <vs-button color="#00b8cf"
-                                       type="filled"
-                                       class="my-round"
-                                       v-else
-                                       @click.prevent="create"
-                            >
-                                Create
-                            </vs-button>
+                            <div>
+                                <vs-button color="#00b8cf"
+                                           type="filled"
+                                           class="my-round"
+                                           @click.prevent="submit"
+                                >Create
+                                </vs-button>
+                            </div>
                         </div>
                         <div class="col-md-8">
                             <ow-data-table :headers="tableHeader"
                                            :tableHeader="'Payment Method List'"
-                                           :suggestText="'Payment Method Record list on table. Filter Payment Method using the filter.'"
-                                           :url="'/json/payment-method'"
-                                           :noDataMessage="'No Payment Method data found. Please Filter Payment Method to show.'"
+                                           :suggestText="'Payment Method Record list on table. Filter Payment Method using the filter.\n'"
+                                           :url="url"
+                                           :noDataMessage="'No Payment Method data found. Please Payment Method  filter to show.'"
                                            :hasSearch="true"
                                            :has-multiple="true"
                                            :has-pagination="true"
+                                           :main-item="mainItem"
+                                           :getData="getData"
                             >
                                 <template slot="items" slot-scope="props">
-                                    <vs-td>
-
+                                    <vs-td :data="props.data.title">
+                                        {{props.data.title+'-['+props.data.id+']'}}
                                     </vs-td>
 
                                     <vs-td>
-                                        <vs-switch color="success"
-                                                   :checked="props.data.status=='Active'?true:false"
-                                                   @click.stop="changeStatus(props.data.id)"
-                                                   class="pointer-all"
-                                        >
-                                            <span slot="on">Active</span>
-                                            <span slot="off">In-Active</span>
-                                        </vs-switch>
+                                        <div class="d-flex flex-wrap">
+                                            <vs-switch color="success"
+                                                       :checked="props.data.status=='active'?true:false"
+                                                       @click.stop="changeStatus(props.data.id,props.data.status)"
+                                                       class="pointer-all ml-2"
+                                            >
+                                                <span slot="on">Active</span>
+                                                <span slot="off">In-Active</span>
+                                            </vs-switch>
+                                        </div>
                                     </vs-td>
-
 
                                     <vs-td>
                                         <div class="action-own">
-
-                                            <a class="btn btn-success btn-sm waves-effect waves-light pointer-all"
+                                            <a class="btn btn-success btn-sm pointer-all"
                                                title="Edit"
-                                               @click.stop="edit(props.data.id)">
+                                               @click.stop="editItems(props.data.id)">
                                                 <i class="fa fa-pencil"></i>
-                                            </a >
-                                            <a class="btn btn-danger btn-sm bootbox-confirm waves-effect waves-light pointer-all"
+                                            </a>
+                                            <a class="btn btn-danger btn-sm pointer-all"
                                                title="Delete"
-                                               @click.stop="delete(props.data.id)">
+                                               @click.stop="deletePopModal(props.data.id)">
                                                 <i class="fa fa-trash-o"></i>
                                             </a>
                                         </div>
                                     </vs-td>
+                                </template>
 
+
+                                <template slot="printSection" slot-scope="printData">
+                                    <thead>
+                                    <tr>
+                                        <th>SN.No.</th>
+                                        <th>
+                                            Student Batch
+                                        </th>
+                                        <th>
+                                            Status
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="(tr, idx) in printData.data">
+                                        <td>
+                                            {{printData.data.indexOf(tr)+1}}
+                                        </td>
+                                        <td>
+                                            {{tr.title+'-['+tr.id+']'}}
+                                        </td>
+                                        <td>
+                                            <div>
+                                                <span v-if="tr.status=='active'" class="p-2 ">Active</span>
+                                                <span v-else class="p-2">In-Active</span>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    </tbody>
                                 </template>
                             </ow-data-table>
                         </div>
@@ -100,41 +134,102 @@
                 </vs-card>
             </div>
         </div>
+        <vs-popup class="holamundo"
+                  :title="'Delete Confirmation'"
+                  :active.sync="deletePop">
+            <div class="mt-3">
+                <p class="p-2 my-round delete-pop-text">These items will be permanently deleted and cannot be
+                    recovered.</p>
+
+                <p><i class="p-2 ace-icon fa fa-hand-o-right"></i>Are you sure?</p>
+            </div>
+
+            <div class="footer-modal">
+                <vs-button class="smBtn"
+                           @click="deletePop=false, deleteItem= null">
+                    <i class="fa fa-close"></i>
+                    Cancel
+                </vs-button>
+                <vs-button class="smBtn" color="danger" @click="deletePop=false, deleteItems()">
+                    <i class="fa fa-trash"></i>
+                    Yes,Delete Now!
+                </vs-button>
+            </div>
+        </vs-popup>
     </div>
 </template>
 
 <script>
 
-
     export default {
+        components: {},
+        name: "payment-method",
         data() {
             return {
+                searchData: {},
                 tableHeader: [
-                    {name: 'Payment Method', sort_key: 'payment_method'},
-                    {name: 'Status', sort_key: ''},
-                    {name: 'Action', sort_key: ''},
+                    {name: 'Payment Method', sort_key: 'title'},
+                    {name: 'Status'},
+                    {name: 'Action'},
                 ],
-                notification: 'Please, Create Year and Active',
-                status: '',
-                status2: [],
-                button: '',
+                title: '',
+                items: [],
+                mainItem: [],
+                deletePop: false,
+                deleteItem: null,
+                url: '/json/payment-method',
             }
         },
+        created() {
+            this.getData()
+        },
+
         methods: {
-            create() {
-                alert(this.status)
+            getData() {
+                this.$http.get(this.url).then(res => {
+                    this.items = res.data.payment_method;
+                    this.mainItem = this.items;
+                })
             },
-            edit(id) {
-                this.$router.push({name:'paymentMethodEdit',params:{id:id}})
+            submit() {
+                this.$validator.validateAll().then(value => {
+                    if (value) {
+                        this.$http.post(this.url + '/store', {
+                            title: this.title,
+                        }).then(res => {
+                            this.$root.notification.status = res.data[0];
+                            this.$root.notification.message = res.data[1];
+                            this.title = '';
+                            this.getData();
+                            this.$validator.reset()
+                        })
+                    }
+                })
             },
-            delete(id) {
+            changeStatus(id, status) {
+                let stat = status === 'active' ? 'in-active' : 'active';
+                let url = '/json/payment-method/' + id + '/' + stat;
+                this.$http.get(url).then(res => {
+                    this.getData();
+                    this.$root.notification.status = res.data[0];
+                    this.$root.notification.message = res.data[1]
+                })
+
             },
-            update(){
-              alert('update')
+            editItems(id) {
+                this.$router.push({name: 'paymentEdit', params: {id: id}})
             },
-            changeStatus() {
-                alert('')
-            }
+            deletePopModal(id) {
+                this.deleteItem = id;
+                this.deletePop = true
+            },
+            deleteItems() {
+                this.$http.get('/json/payment-method/' + this.deleteItem + '/delete').then(res => {
+                    this.getData();
+                    this.$root.notification.status = res.data[0];
+                    this.$root.notification.message = res.data[1]
+                })
+            },
         }
     }
 </script>

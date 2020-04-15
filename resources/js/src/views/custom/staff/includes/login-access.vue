bd0f8a07a8a5591ebf98e651acdf4ceb79d2253c
MicroDreamIT <shahanur700@gmail.com>
2020-04-15 17:09:36 +0600

    staff login

--- resources/js/src/views/custom/staff/includes/login-access.vue
+++ resources/js/src/views/custom/staff/includes/login-access.vue
@@ -1,8 +1,20 @@
 <template>
     <div class="row">
+        <div class="col-md-12" v-if="item.staff_login">
+            <h4 class="header large lighter blue"><i class="fa fa-key" aria-hidden="true"></i>&nbsp;Edit Staff Login Access</h4>
+            <div>
+                <a @click="userAction(item.staff_login.id, 'active')"  title="Active" class="btn-success btn-sm"><i class="fa fa-unlock-alt" aria-hidden="true" ></i> Un-Lock User</a>
 
-        <div v-if="!item.staff_login">
-            <h4 class="header large lighter blue"><i class="fa fa-key" aria-hidden="true"></i>&nbsp;Create Staff Login Access</h4>
+                <a @click="userAction(item.staff_login.id, 'in-active')"  title="In-Active" class="btn-warning btn-sm"><i class="fa fa-lock" aria-hidden="true" ></i> Lock User</a>
+
+                <a @click="userAction(item.staff_login.id, 'delete')"  title="Delete" class="btn-danger btn-sm"><i class="fa fa-trash" aria-hidden="true" ></i> Delete User</a>
+
+            </div>
+            <br>
+        </div>
+        <div class="col-md-12">
+            <h4 class="header large lighter blue" v-if="!item.staff_login"><i class="fa fa-key" aria-hidden="true" ></i>&nbsp;Create Staff Login Access</h4>
+            <input type="hidden" v-model="login.id" v-if="item.staff_login">
             <input type="hidden" v-model="login.role_id">
             <input type="hidden" v-model="login.hook_id">
             <div class="form-group">
@@ -29,77 +41,11 @@
 
                 <button class="btn btn-info" type="submit" @click="posting()">
                     <i class="fa fa-save bigger-110"></i>
-                    Create Login Access
+                    {{buttonText}} Login Access
                 </button>
             </div>
         </div>
-<!--        <div class="col-xs-12" v-else>-->
-<!--            <h4 class="header large lighter blue"><i class="fa fa-key" aria-hidden="true"></i>&nbsp;Edit Staff Login Access</h4>-->
-<!--            -->
-<!--            <a href="{{ route('staff.user.active', ['id' => $data['staff_login']->id]) }}" title="Active" class="btn-success btn-sm"><i class="fa fa-unlock-alt" aria-hidden="true"></i> Un-Lock User</a>-->
-<!--            <a href="{{ route('staff.user.in-active', ['id' => $data['staff_login']->id]) }}" title="In-Active" class="btn-warning btn-sm"><i class="fa fa-lock" aria-hidden="true"></i> Lock User</a>-->
-<!--            <a href="{{ route('staff.user.delete', ['id' => $data['staff_login']->id]) }}" title="Delete" class="btn-danger btn-sm"><i class="fa fa-trash" aria-hidden="true"></i> Delete User</a>-->
-<!--            <div class="hr hr-24"></div>-->
 
-<!--            {!! Form::model($data['staff_login'], ['route' => ['staff.user.update', $data['staff_login']->id], 'method' => 'POST', 'class' => 'form-horizontal',-->
-<!--            'id' => 'validation-form', "enctype" => "multipart/form-data"]) !!}-->
-<!--            {!! Form::hidden('id', $data['staff_login']->id) !!}-->
-<!--            {!! Form::hidden('role_id', 5) !!}-->
-<!--            {!! Form::hidden('hook_id', $data['staff']->id) !!}-->
-
-<!--            <div class="form-group">-->
-<!--                {!! Form::label('name', 'Name', ['class' => 'col-sm-2 control-label']) !!}-->
-<!--                <div class="col-sm-4">-->
-<!--                    {!! Form::text('name', null, ["placeholder" => "", "class" => "form-control border-form", "required"]) !!}-->
-<!--                    @include('includes.form_fields_validation_message', ['name' => 'name'])-->
-<!--                </div>-->
-
-<!--                {!! Form::label('email', 'Email', ['class' => 'col-sm-2 control-label']) !!}-->
-<!--                <div class="col-sm-4">-->
-<!--                    {!! Form::email('email', null, ["placeholder" => "", "class" => "form-control border-form", "required"]) !!}-->
-<!--                    @include('includes.form_fields_validation_message', ['name' => 'email'])-->
-<!--                </div>-->
-<!--            </div>-->
-
-<!--            <div class="form-group">-->
-<!--                {!! Form::label('password', 'Password', ['class' => 'col-sm-2 control-label']) !!}-->
-<!--                <div class="col-sm-4">-->
-<!--                    {!! Form::password('password',  ["placeholder" => "", "class" => "form-control border-form", "id"=>"pass", "required"]) !!}-->
-<!--                    @include('includes.form_fields_validation_message', ['name' => 'password'])-->
-<!--                </div>-->
-
-<!--                {!! Form::label('confirmPassword', 'Confirm Password', ['class' => 'col-sm-2 control-label']) !!}-->
-<!--                <div class="col-sm-4">-->
-<!--                    {!! Form::password('confirmPassword',  ["placeholder" => "", "class" => "form-control border-form"/*,"onkeyup"=>"passCheck()"*/,"id"=>"repatpass", "required"]) !!}-->
-<!--                    @include('includes.form_fields_validation_message', ['name' => 'confirmPassword'])-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <div class="col-sm-4">-->
-<!--                <label data-toggle="dropdown" class="label {{ $data['staff_login']->status == 'active'?"label-success":"label-danger" }}" >-->
-<!--                {{ $data['staff_login']->status == 'active'?"Active User":"User Locked" }}-->
-<!--                </label>-->
-<!--            </div>-->
-<!--            <div class="clearfix hr-8"></div>-->
-
-<!--            <div class="clearfix form-actions">-->
-<!--                <div class="col-md-12 align-right">-->
-<!--                    <button class="btn" type="reset">-->
-<!--                        <i class="fa fa-undo bigger-110"></i>-->
-<!--                        Reset-->
-<!--                    </button>-->
-
-<!--                    <button class="btn btn-info" type="submit">-->
-<!--                        <i class="fa fa-save bigger-110"></i>-->
-<!--                        Update Detail-->
-<!--                    </button>-->
-<!--                </div>-->
-<!--            </div>-->
-
-
-<!--            <div class="hr hr-24"></div>-->
-<!--            {!! Form::close() !!}-->
-<!--        </div>-->
-<!--        @endif-->
 
     </div>
 </template>
@@ -115,18 +61,36 @@
                 login:{
                     role_id:null,
                     hook_id:null,
-                }
+                },
+                buttonText:'create'
             }
         },
         created() {
+            if (this.item.staff_login) {
+                this.login.id = this.item.staff_login.id
+                this.buttonText = 'update'
+            }
+
             this.login.name=this.item.staff.fullname
             this.login.email = this.item.staff.email
             this.login.hook_id = parseInt(this.item.staff.id)
             this.login.role_id = parseInt(5)
+
         },
         methods:{
+            userAction(id, action){
+                this.$http.get('/json/staff/' + id + '/user/' + action)
+                    .then(res=>{
+                        if(res.status===200){
+                            this.$vs.notify({title:res.data[0],text:res.data[1],color:res.data[0],icon:'verified_user'})
+                            this.$emit('getData')
+                        }
+                    })
+            },
             posting(){
-                this.$http.post('/json/staff/user/create', this.login)
+                let url = this.item.staff_login? '/json/staff/' + this.item.staff_login.id  + '/user/update' : '/json/staff/user/create'
+
+                this.$http.post(url, this.login)
                     .then(res=>{
                         if(res.status===200){
                             this.$vs.notify({title:res.data[0],text:res.data[1],color:res.data[0],icon:'verified_user'})

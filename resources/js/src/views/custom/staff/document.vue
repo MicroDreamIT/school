6227f403cd1c7cdc89fbf170af2b34f69363fd20
MicroDreamIT <shahanur700@gmail.com>
2020-04-16 19:29:08 +0600

    staff edit

--- resources/js/src/views/custom/staff/document.vue
+++ resources/js/src/views/custom/staff/document.vue
@@ -4,81 +4,7 @@
             <div class="col-md-12 mb-2">
                 <h2 class="pageTitle">Staff Manager</h2>
             </div>
-            <div class="col-md-12">
-                <div class="row mx-0">
-                    <router-link :to="'/staff'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-list" aria-hidden="true"></i>
-                            Detail
-                        </vs-button>
-                    </router-link>
-                     <router-link :to="'/staff/add'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-plus" aria-hidden="true"></i>
-                            Registration
-                        </vs-button>
-                    </router-link>
-                    <router-link :to="'/staff/import'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-upload" aria-hidden="true"></i>
-                            Bulk Registration
-                        </vs-button>
-                    </router-link>
-
-                    <router-link :to="'/staff/document'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-files-o" aria-hidden="true"></i>
-                            Documents
-                        </vs-button>
-                    </router-link>
-                    <router-link :to="'/staff/note'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-sticky-note" aria-hidden="true"></i>
-                            Notes
-                        </vs-button>
-                    </router-link>
-                    <router-link :to="'/staff/payroll'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-sticky-note" aria-hidden="true"></i>
-                            Payroll
-                        </vs-button>
-                    </router-link>
-                    <router-link :to="'/library/staff'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-calculator" aria-hidden="true"></i>
-                            Library
-                        </vs-button>
-                    </router-link>
-                    <router-link :to="'/attendance/staff'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-calendar" aria-hidden="true"></i>
-                            Attendance
-                        </vs-button>
-                    </router-link>
-                    <router-link :to="'/staff/designation'">
-                        <vs-button type="filled" class="smBtn">
-                            <i class="fa fa-calendar" aria-hidden="true"></i>
-                            Designation
-                        </vs-button>
-                    </router-link>
-                </div>
-            </div>
-            <div class="col-md-12" v-if="notification">
-                <div role="alert"
-                     class="mt-2 alert alert-success alert-dismissible display-block"
-                >
-                    <button type="button"
-                            data-dismiss="alert"
-                            aria-label="Close"
-                            class="close"
-                            @click="notification=''"
-                    >
-                        <span aria-hidden="true">×</span>
-                    </button>
-                    <i class="ace-icon fa fa-hand-o-right"></i>
-                    {{notification}}
-                </div>
-            </div>
+            <staff-navigation></staff-navigation>
             <vs-divider class="mx-3"/>
             <div class="col-md-12">
                 <vs-card>
@@ -188,9 +114,12 @@
 </template>
 
 <script>
+    import StaffNavigation from '../../components/navigation/staff-navigation.vue'
 
     export default {
-
+        components: {
+            'staff-navigation': StaffNavigation
+        },
         data() {
             return {
 
@@ -204,7 +133,13 @@
 				document:{}
             }
         },
+        created(){
+
+        },
         methods: {
+            getData(){
+
+            },
             viewItems(id) {
                 this.$router.push({name: 'studentView', params: {id: id}})
             },

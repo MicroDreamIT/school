9af4eb2b2a88dac67c3ba796fe8182855d90e7a8
MicroDreamIT <shahanur700@gmail.com>
2020-05-06 20:22:04 +0600

    member

--- resources/js/src/views/custom/library/members/student.vue
+++ resources/js/src/views/custom/library/members/student.vue
@@ -45,6 +45,136 @@
 
                 </div>
             </div>
+            <div class="col-md-12 mb-2" v-if="filterSection">
+                <vs-collapse class="custom-collapse">
+                    <vs-collapse-item>
+                        <div slot="header">
+                            <vs-button type="filled"
+                                       color="primary"
+                                       icon="double_arrow"
+                                       class="rounded"
+                            >
+                                Filter Students
+                            </vs-button>
+                        </div>
+                        <div class="filterBox">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <div class="form-group ">
+                                            <label>Reg:</label>
+                                            <vs-input v-model="searchData.reg_no" class="w-100">
+                                            </vs-input>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Batch</label>
+                                            <v-select :options="batch"
+                                                      v-model="searchData.batch">
+                                            </v-select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Reg Date From</label>
+                                            <datepicker
+                                                    :value="searchData.reg_start_date"
+                                                    :format="'yyyy-MM-dd'"
+                                                    @input="searchData.reg_start_date=$root.formatPicker($event)"
+                                            />
+                                            <label>To</label>
+                                            <datepicker
+                                                    v-model="searchData.reg_end_date"
+                                                    :format="'yyyy-MM-dd'"
+                                                    @input="searchData.reg_end_date=$root.formatPicker($event)"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Academic Status:</label>
+                                            <v-select v-model="searchData.academic_status"
+                                                      :options="academic_status"
+                                                      label="value"
+                                                      :reduce="a => a.id"
+                                                      placeholder="Select Academic Status"
+                                            >
+                                            </v-select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Status:</label>
+                                            <v-select v-model="searchData.status"
+                                                      :options="['Active','In-Active']"
+                                                      placeholder="Select Status"
+                                            >
+                                            </v-select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Faculty/Class</label>
+                                            <v-select v-model="searchData.faculty"
+                                                      :options="faculties"
+                                                      label="value"
+                                                      :reduce="a => a.id"
+                                                      placeholder="Select Faculty/Class"
+                                                      @input="findSemester(searchData.faculty)"
+                                            >
+                                            </v-select>
+
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Sem./Sec.</label>
+                                            <v-select v-model="searchData.semester_select"
+                                                      :options="semester"
+                                                      label="semester"
+                                                      :reduce="a => a.id"
+                                                      placeholder="Select Sem./Sec."
+                                            >
+                                            </v-select>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="row  mb-3">
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Religion:</label>
+                                            <vs-input v-model="searchData.religion" class="w-100"></vs-input>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Caste:</label>
+                                            <vs-input v-model="searchData.caste" class="w-100"></vs-input>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Nationality:</label>
+                                            <vs-input v-model="searchData.nationality" class="w-100"></vs-input>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                            <label>Mot.Tongue:</label>
+                                            <vs-input v-model="searchData.mother_tongue" class="w-100"></vs-input>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-2 pl-0">
+                                <vs-button type="filled"
+                                           color="#00b8cf"
+                                           icon="double_arrow"
+                                           @click.prevent="doFilter"
+                                >
+                                    Filter
+                                </vs-button>
+                            </div>
+                        </div>
+                    </vs-collapse-item>
+                </vs-collapse>
+            </div>
             <vs-divider class="mx-3"/>
             <div class="col-md-12">
                 <vs-card>
@@ -55,6 +185,7 @@
                                       :model="'route'"
                                       :noDataMessage="'No Student data found. Please Filter student to show.'"
                                       :hasSearch="true"
+                                      :searchData="searchData"
                                       :has-multiple="true"
                                       :has-pagination="true"
                                       :filterSection="true"
@@ -62,10 +193,29 @@
                                       :ajaxVariableSet="['student']"
                                       @get-return-value="GetReturnValue"
                                       :showAction="false"
+                                      :showStatus="false"
                     >
                         <template slot="items" slot-scope="props">
                             <vs-td :data="props.data.faculty">
                                 {{props.data.faculty}}
+                            </vs-td>
+                            <vs-td :data="props.data.semester">
+                                {{props.data.semester}}
+                            </vs-td>
+                            <vs-td :data="props.data.reg_no">
+                                {{props.data.reg_no}}
+                            </vs-td>
+                            <vs-td :data="props.data.fullname">
+                                {{props.data.fullname}}
+                            </vs-td>
+                            <vs-td :data="props.data.taken">
+                                {{props.data.taken}}
+                            </vs-td>
+                            <vs-td :data="props.data.eligible">
+                                {{props.data.eligible}}
+                            </vs-td>
+                            <vs-td :data="props.data.status">
+                                {{props.data.status}}
                             </vs-td>
 
                         </template>
@@ -83,6 +233,8 @@
 
         data() {
             return {
+                filterSection:true,
+                searchData:{},
                 headers: [
                     {name: 'Faculty/Class', sort_key: 'faculty'},
                     {name: 'Sem/Sec', sort_key: 'sem_sec'},
@@ -96,6 +248,15 @@
             }
         },
 
+        watch:{
+            searchData:{
+                deep:true,
+                handler(val){
+                    console.log(val)
+                    this.$refs.dataTableStudent.getData()
+                }
+            }
+        },
         created() {
 
         },
@@ -108,7 +269,13 @@
                         id: st.id,
                         faculty:faculty.filter(f=>{
                             return f.id === st.faculty
-                        })[0].value
+                        })[0].value,
+                        semester:st.semester,
+                        reg_no:st.memberdetail[2],
+                        taken:st.taken,
+                        eligible:st.eligible,
+                        status:st.status,
+                        fullname:this.$root.singleSpacing(st.first_name+' '+st.middle_name+' '+st.last_name)
                     }
                 });
                 this.$store.dispatch('updateTableData', val)
